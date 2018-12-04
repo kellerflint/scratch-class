bfaa1fcb7a4b5259c1e2ca77e6c4270445e053a2
unknown <kflint0068@gmail.com>
2018-12-04 10:11:08 -0800

    Added Progress page and logic; added nav menu

---
+++ script.js
@@ -0,0 +1,56 @@
+let achievements = [
+    // Level 0
+    ["Animate Name", "Make Music", "Pong Game"],
+    // Level 1
+    ["Make It Fly", "Create Story"],
+    // Level 2
+    []
+];
+
+let students = [
+    {
+        name: "Amber",
+        student_achievements: []
+    },
+    {
+        name: "Cooper",
+        student_achievements: []
+    },
+    {
+        name: "Ethan",
+        student_achievements: []
+    },
+    {
+        name: "Sergiu",
+        student_achievements: []
+    }
+];
+
+// Amber
+//students[0].student_achievements.push(achievements[0][1]);
+
+// Cooper
+
+// Ethan
+
+// Sergiu
+
+for (let i in students) {
+    let student_bar = students[i].name.toLocaleLowerCase();
+    student_bar += "-bar";
+
+    for (let c in students[i].student_achievements) {
+        let progress_item = document.createElement("div");
+        progress_item.classList.add("progress-item");
+
+        let text = document.createTextNode(students[i].student_achievements[c]);
+        progress_item.appendChild(text);
+
+        document.getElementById(student_bar).appendChild(progress_item);
+    }
+
+
+
+
+
+}
