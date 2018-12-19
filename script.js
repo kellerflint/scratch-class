ed60bffefff7d354ecf7e0825ddfe9d8ec13a9e3
kellerflint <kflint0068@gmail.com>
2018-12-18 19:30:59 -0800

    updated progress

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ let achievements = [
     // Level 1
     ["Make It Fly", "Create Story", "Pong Game", "Level 1 Test"],
     // Level 2
-    ["Catch Game", "TODO", "TODO", "Level 2 Test"]
+    ["Catch Game", "TODO", "TODO", "Level 2 Test"],
+    // Level 3
+    ["Project #1", "Project #2"]
 ];
 
 let students = [
@@ -36,12 +38,15 @@ students[0].student_achievements.push(achievements[1][0]);
 students[0].student_achievements.push(achievements[1][1]);
 students[0].student_achievements.push(achievements[2][0]);
 students[0].student_achievements.push(achievements[0][4]);
+students[0].student_achievements.push(achievements[3][0]);
+students[0].student_achievements.push(achievements[1][3]);
 
 
 // Cooper
 
 // Ethan
 students[2].student_achievements.push(achievements[1][2]);
+students[2].student_achievements.push(achievements[0][3]);
 
 // Sergiu
 students[3].student_achievements.push(achievements[0][1]);
@@ -49,6 +54,9 @@ students[3].student_achievements.push(achievements[0][3]);
 students[3].student_achievements.push(achievements[1][2]);
 students[3].student_achievements.push(achievements[0][1]);
 students[3].student_achievements.push(achievements[0][2]);
+students[3].student_achievements.push(achievements[2][0]);
+students[3].student_achievements.push(achievements[1][0]);
+students[3].student_achievements.push(achievements[0][4]);
 
 
 for (let i in students) {
